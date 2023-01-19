f40ef7dc46ef3c0f4860d7b9f1fe072e53f696c6
Worldline Direct support team <82139942+worldline-direct-support-team@users.noreply.github.com>
2023-01-19 15:40:16 +0100

    Release version 4.6.0.

--- src/methods/services/index.js
+++ src/methods/services/index.js
@@ -1,11 +1,13 @@
 /* This file was auto-generated. */
 
+const surchargeCalculation = require('./surchargeCalculation');
 const getPrivacyPolicy = require('./getPrivacyPolicy');
 const testConnection = require('./testConnection');
 const getIINDetails = require('./getIINDetails');
 
 module.exports = function (sdkContext) {
   return {
+    surchargeCalculation: surchargeCalculation(sdkContext),
     getPrivacyPolicy: getPrivacyPolicy(sdkContext),
     testConnection: testConnection(sdkContext),
     getIINDetails: getIINDetails(sdkContext),
---
+++ src/methods/services/surchargeCalculation.js
@@ -0,0 +1,22 @@
+/* This file was auto-generated. */
+const communicator = require('../../utils/communicator');
+const requestSchema = require('../../schemas/CalculateSurchargeRequest.json');
+const validator = require('../../utils/validator');
+
+module.exports = (sdkContext) => {
+  return function (merchantId, postData, paymentContext, cb = null) {
+    // validate postData
+    validator.validatePostData(postData, requestSchema, sdkContext);
+
+    communicator.json(
+      {
+        method: 'POST',
+        modulePath: `/v2/${merchantId}/services/surchargecalculation`,
+        body: postData,
+        paymentContext,
+        cb,
+      },
+      sdkContext
+    );
+  };
+};
