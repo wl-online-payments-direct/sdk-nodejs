28643bcd43564c1e41d9464acd2c5431fc821f66
Worldline Direct support team <82139942+worldline-direct-support-team@users.noreply.github.com>
2023-05-30 15:05:05 +0200

    Release version 4.10.0.

--- src/methods/services/index.js
+++ src/methods/services/index.js
@@ -1,6 +1,7 @@
 /* This file was auto-generated. */
 
 const surchargeCalculation = require('./surchargeCalculation');
+const getDccRateInquiry = require('./getDccRateInquiry');
 const getPrivacyPolicy = require('./getPrivacyPolicy');
 const testConnection = require('./testConnection');
 const getIINDetails = require('./getIINDetails');
@@ -8,6 +9,7 @@ const getIINDetails = require('./getIINDetails');
 module.exports = function (sdkContext) {
   return {
     surchargeCalculation: surchargeCalculation(sdkContext),
+    getDccRateInquiry: getDccRateInquiry(sdkContext),
     getPrivacyPolicy: getPrivacyPolicy(sdkContext),
     testConnection: testConnection(sdkContext),
     getIINDetails: getIINDetails(sdkContext),
