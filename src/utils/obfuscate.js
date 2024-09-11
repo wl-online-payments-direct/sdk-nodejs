938c74a5b5c880e1c0a3f20c38660ab08733eba8
Worldline Direct support team <82139942+worldline-direct-support-team@users.noreply.github.com>
2024-09-11 16:24:06 +0200

    Release version 4.23.0.

--- src/utils/obfuscate.js
+++ src/utils/obfuscate.js
@@ -1,25 +1,114 @@
 /* eslint-disable no-param-reassign, global-require, no-unused-vars */
-const traverse = require('traverse');
 
 const INDENT = 2;
+const REPLACECHAR = '*';
 
-/** Replaces a value with a '*' followed by the length of the value. */
-const obfuscateValue = function (json, property) {
-  traverse(json).forEach(function (node) {
-    if (this.key === property) {
-      node = `${node}`; // make sure it's a string
-      const l = node.length.toString();
-      this.update(`*${l}`);
-    }
+/**
+ * @returns An obfuscation rule that will replace all characters with *.
+ */
+function obfuscateAll() {
+  const rule = () => {
+    return REPLACECHAR.repeat(3);
+  };
+  return rule;
+}
+
+/**
+ * @returns An obfuscation rule that will replace with * and total lenght (e.g. AB1 -> *3).
+ */
+function obfuscateWithLength() {
+  const rule = (_value) => {
+    const l = `${_value}`.length.toString();
+    return `${REPLACECHAR}${l}`;
+  };
+  return rule;
+}
+
+/**
+ * @return All the obfuscation rules.
+ */
+function getObfuscateRules() {
+  const obfuscationRules = {};
+
+  obfuscationRules.accountNumber = obfuscateWithLength();
+  obfuscationRules.bin = obfuscateWithLength();
+  obfuscationRules.cardNumber = obfuscateWithLength();
+  obfuscationRules.cvv = obfuscateWithLength();
+  obfuscationRules.expiryDate = obfuscateWithLength();
+  obfuscationRules.iban = obfuscateWithLength();
+  obfuscationRules.reformattedAccountNumber = obfuscateWithLength();
+  // GDPR-related fields
+  obfuscationRules.additionalInfo = obfuscateWithLength();
+  obfuscationRules.cardholderName = obfuscateWithLength();
+  obfuscationRules.dateOfBirth = obfuscateWithLength();
+  obfuscationRules.emailAddress = obfuscateWithLength();
+  obfuscationRules.faxNumber = obfuscateWithLength();
+  obfuscationRules.firstName = obfuscateWithLength();
+  obfuscationRules.houseNumber = obfuscateWithLength();
+  obfuscationRules.mobilePhoneNumber = obfuscateWithLength();
+  obfuscationRules.passengerName = obfuscateWithLength();
+  obfuscationRules.phoneNumber = obfuscateWithLength();
+  obfuscationRules.street = obfuscateWithLength();
+  obfuscationRules.surname = obfuscateWithLength();
+  obfuscationRules.workPhoneNumber = obfuscateWithLength();
+  obfuscationRules.zip = obfuscateWithLength();
+  // key-value pairs can contain any value, like credit card numbers or other private data; mask all values
+  obfuscationRules.value = obfuscateAll();
+
+  obfuscationRules.keyId = obfuscateAll();
+  obfuscationRules.secretKey = obfuscateAll();
+  obfuscationRules.publicKey = obfuscateAll();
+  obfuscationRules.userAuthenticationToken = obfuscateAll();
+  // encrypted payload is a base64 string that contains an encrypted value; to make decrypting even harder, just mask the entire thing
+  obfuscationRules.encryptedPayload = obfuscateAll();
+  // decrypted payload is a simple base64 string that may contain credit card numbers or other private data; just mask the entire thing
+  obfuscationRules.decryptedPayload = obfuscateAll();
+  // encrypted customer input is similar to encrypted payload
+  obfuscationRules.encryptedCustomerInput = obfuscateAll();
+
+  // headers
+  obfuscationRules.Authorization = obfuscateAll();
+  obfuscationRules['WWW-Authenticate'] = obfuscateAll();
+  obfuscationRules['Proxy-Authenticate'] = obfuscateAll();
+  obfuscationRules['Proxy-Authorization'] = obfuscateAll();
+  obfuscationRules['X-GCS-Authentication-Token'] = obfuscateAll();
+  obfuscationRules['X-GCS-CallerPassword'] = obfuscateAll();
+
+  return obfuscationRules;
+}
+
+/**
+ * @param {*} value
+ * @param {*} obfuscationRule
+ * @returns the value obfuscated based on the obfuscation rule
+ */
+function applyObfuscationRule(value, obfuscationRule) {
+  return obfuscationRule ? obfuscationRule(`${value}`) : value;
+}
+
+/**
+ * @param {*} json
+ * @param {*} obfuscationRules
+ * @returns a new json based on the obfuscation rule
+ */
+function applyObfuscationRules(json, obfuscationRules) {
+  if (json === null || typeof json !== 'object') {
+    return json;
+  }
+  if (Array.isArray(json)) {
+    return json.map((value) => applyObfuscationRules(value, obfuscationRules));
+  }
+
+  const result = {};
+  Object.entries(json).forEach(([key, value]) => {
+    const newValue =
+      value !== null && typeof value === 'object'
+        ? applyObfuscationRules(value, obfuscationRules)
+        : applyObfuscationRule(value, obfuscationRules[key]);
+    result[key] = newValue;
   });
-};
-const obfuscateSensitiveValue = function (json, property) {
-  traverse(json).forEach(function () {
-    if (this.key === property) {
-      this.update('***');
-    }
-  });
-};
+  return result;
+}
 
 const obfuscate = {
   getObfuscated(input, context) {
@@ -36,50 +125,10 @@ const obfuscate = {
         return input;
       }
     }
-    const obfuscated = JSON.parse(JSON.stringify(input));
-    obfuscateValue(obfuscated, 'accountNumber');
-    obfuscateValue(obfuscated, 'bin');
-    obfuscateValue(obfuscated, 'cardNumber');
-    obfuscateValue(obfuscated, 'cvv');
-    obfuscateValue(obfuscated, 'expiryDate');
-    obfuscateValue(obfuscated, 'iban');
-    obfuscateValue(obfuscated, 'reformattedAccountNumber');
-    // GDPR-related fields
-    obfuscateValue(obfuscated, 'additionalInfo');
-    obfuscateValue(obfuscated, 'cardholderName');
-    obfuscateValue(obfuscated, 'dateOfBirth');
-    obfuscateValue(obfuscated, 'emailAddress');
-    obfuscateValue(obfuscated, 'faxNumber');
-    obfuscateValue(obfuscated, 'firstName');
-    obfuscateValue(obfuscated, 'houseNumber');
-    obfuscateValue(obfuscated, 'mobilePhoneNumber');
-    obfuscateValue(obfuscated, 'passengerName');
-    obfuscateValue(obfuscated, 'phoneNumber');
-    obfuscateValue(obfuscated, 'street');
-    obfuscateValue(obfuscated, 'surname');
-    obfuscateValue(obfuscated, 'workPhoneNumber');
-    obfuscateValue(obfuscated, 'zip');
-    // key-value pairs can contain any value, like credit card numbers or other private data; mask all values
-    obfuscateValue(obfuscated, 'value');
 
-    obfuscateSensitiveValue(obfuscated, 'keyId');
-    obfuscateSensitiveValue(obfuscated, 'secretKey');
-    obfuscateSensitiveValue(obfuscated, 'publicKey');
-    obfuscateSensitiveValue(obfuscated, 'userAuthenticationToken');
-    // encrypted payload is a base64 string that contains an encrypted value; to make decrypting even harder, just mask the entire thing
-    obfuscateSensitiveValue(obfuscated, 'encryptedPayload');
-    // decrypted payload is a simple base64 string that may contain credit card numbers or other private data; just mask the entire thing
-    obfuscateSensitiveValue(obfuscated, 'decryptedPayload');
-    // encrypted customer input is similar to encrypted payload
-    obfuscateSensitiveValue(obfuscated, 'encryptedCustomerInput');
-
-    // headers
-    obfuscateSensitiveValue(obfuscated, 'Authorization');
-    obfuscateSensitiveValue(obfuscated, 'WWW-Authenticate');
-    obfuscateSensitiveValue(obfuscated, 'Proxy-Authenticate');
-    obfuscateSensitiveValue(obfuscated, 'Proxy-Authorization');
-    obfuscateSensitiveValue(obfuscated, 'X-GCS-Authentication-Token');
-    obfuscateSensitiveValue(obfuscated, 'X-GCS-CallerPassword');
+    const data = JSON.parse(JSON.stringify(input));
+    const obfuscationRules = getObfuscateRules();
+    const obfuscated = applyObfuscationRules(data, obfuscationRules);
 
     return JSON.stringify(obfuscated, null, INDENT);
   },
